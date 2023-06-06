9b0a096b6e49b6841e3a99565813d82d3616f0ac
Lorne Prince <lorneAprince@gmail.com>
2023-06-06 10:08:02 -0400

    Add project files.

---
+++ CBALoginChecker/LoginVerifier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBALoginChecker
+{
+    public class LoginVerifier
+    {
+        public bool IsLoginValid(string username, string password)
+        {
+            bool unameReadFromFile = false;
+            string fileName = @"e:\loginfile.txt";
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (!unameReadFromFile)
+                        {
+                            if (line.ToLower() != username.ToLower())
+                            {
+                                // the username doesn't match so login failed
+                                return false;
+                            }
+                            unameReadFromFile = true;
+                            continue;
+                        }
+
+                        if (line != password)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CBALoginChecker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CBALoginChecker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var loginVerifierObj = new LoginVerifier();
+
+                Console.Write("Please enter your username: ");
+                var uname = Console.ReadLine();
+
+                Console.Write("\n\n");
+
+                Console.Write("Please enter your password: ");
+                var pwd = Console.ReadLine();
+
+                if(loginVerifierObj.IsLoginValid(uname, pwd))
+                {
+                    Console.WriteLine("\n\n\nCongratulations, you are now logged in.");
+                }
+                else
+                {
+                    Console.WriteLine("\n\n\nSorry, your login information is invalid. Please try again");
+                }
+                Console.ReadLine();
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
